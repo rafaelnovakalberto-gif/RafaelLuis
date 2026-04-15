d719cebf7e852955d65a4fa60f86497d184f59ed
Rafael <rafaelnovakalberto@gmail.com>
2026-04-14 21:10:48 -0300

    Etapa 2 (Funcao guardar_dado): FEITA

--- funcoes.py
+++ funcoes.py
@@ -7,3 +7,15 @@ def rolar_dados(numero_dados):
         lista.append(valor)
 
     return lista
+
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
+    lista_restantes = []
+    for i in range(len(dados_rolados)):
+        if i == dado_para_guardar:
+            continue
+        lista_restantes.append(dados_rolados[i])
+    estoque_novo = dados_no_estoque
+    estoque_novo.append(dados_rolados[dado_para_guardar])
+
+    return [lista_restantes,estoque_novo]
+    
