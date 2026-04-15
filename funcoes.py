40ee4a4de26f11b209b8a1fc60d81848d2472da3
Rafael <rafaelnovakalberto@gmail.com>
2026-04-14 21:14:40 -0300

    Etapa 4 (Funcao calcula_pontos_regra_simples): FEITA

--- funcoes.py
+++ funcoes.py
@@ -30,3 +30,17 @@ def remover_dado(dados_rolados, dados_no_estoque, dado_para_remover):
     
     return [dados_rolados,novo_estoque]
 
+def calcula_pontos_regra_simples(dados_rolados):
+    saida = {
+        1:0,
+        2:0,
+        3:0,
+        4:0,
+        5:0,
+        6:0,
+    }
+    for valor in dados_rolados:
+
+        saida[valor] += valor
+    
+    return saida
